25b4d3cc3d8234496453fc932d3dc7a9471bb74a
unorji kingsley <unorjikingsley@gmail.com>
2023-09-04 20:57:56 +0100

    Solved all linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :foods
   has_many :recipes
+
+  # validates :name, presence: true
+  # validates :email, presence: true, uniqueness: true
 end
