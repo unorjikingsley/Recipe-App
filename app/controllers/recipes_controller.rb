2df9609405ae63fb7de23648cbc4a62e4b078948
unorji kingsley <unorjikingsley@gmail.com>
2023-09-05 19:30:32 +0100

    Genrate shopping list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,25 +2,26 @@ class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
 
   def index
-    @current_user = current_user
-    if @current_user.nil?
+    @user = current_user
+    if @user.nil?
       redirect_to user_session_path, flash: { alert: 'You must be signed in to continue.' }
     else
-      @recipes = @current_user.recipes
+      @recipes = @user.recipes
     end
   end
 
   def new
     @recipe = Recipe.new
-    @foods = Food.all
+    @foods_to_buy = Food.all
     @recipe.recipe_foods.build
+    @user = current_user
   end
 
   def show
     @recipe = Recipe.find_by_id(params[:id])
     @recipe_foods = @recipe&.recipe_foods
     @recipe = 'No recipes' if @recipe.nil?
-    @current_user = current_user
+    @user = current_user
   end
 
   def create
@@ -43,9 +44,71 @@ class RecipesController < ApplicationController
     redirect_to recipes_path
   end
 
+  # The public_recipes action retrieves a list of recipes marked as public (public: true)
+  # and assigns them to @recipes.
   def public_recipes
     @recipes = Recipe.where(public: true)
   end
+
+  # The toggle_public action is used to toggle the "public" status of a recipe. It finds
+  # the recipe by its :recipe_id parameter, toggles its public attribute (changing it
+  # from true to false or vice versa), and then redirects to the recipe's show page.
+  def toggle_public
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe.toggle!(:public)
+    redirect_to recipe_path(@recipe)
+  end
+
+  # generating a general shopping list based on the user's recipes and inventory of foods.
+  # This action appears to calculate the foods to buy and inventory adjustments, and then
+  # renders a shopping list template.
+
+  # rubocop:disable Metrics
+  def general_shopping_list
+    @user = current_user
+    @recipes = @user.recipes
+    @foods_to_buy = {}
+    @inventory = @user.foods
+
+    # @recipes.each do |recipe|
+    #   recipe.recipe_foods.each do |recipe_food|
+    #     if @foods_to_buy.key?(recipe_food.food.name)
+    #       @foods_to_buy[recipe_food.food.name].quantity += recipe_food.quantity
+    #     else
+    #       @foods_to_buy[recipe_food.food.name] = recipe_food
+    #     end
+    #   end
+    # end
+    #
+    # subtract inventory from shopping list
+
+    @recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        if @foods_to_buy.key?(recipe_food.food.name)
+          @foods_to_buy[recipe_food.food.name].quantity += recipe_food.quantity
+        else
+          @foods_to_buy[recipe_food.food.name] = recipe_food
+        end
+      end
+    end
+
+    puts 'Foods to buy: '
+    @foods_to_buy.each do |key, value|
+      puts "#{key}: #{value.quantity}"
+    end
+
+    @inventory.each do |inventory_food|
+      @foods_to_buy[inventory_food.name].quantity -= inventory_food.quantity if @foods_to_buy.key?(inventory_food.name)
+    end
+
+    # remove foods with quantity <= 0
+    @foods_to_buy.each do |key, value|
+      @foods_to_buy.delete(key) if value.quantity <= 0
+    end
+
+    render 'shopping-list/index'
+  end
+  # rubocop:enable Metrics
 
   private
 
--- app/models/food.rb
+++ app/models/food.rb
@@ -6,5 +6,5 @@ class Food < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :measurement_unit, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { greater_than: 0 }
-  # validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
